48d476658f4da8c20e77cc039520431786a9878a
横井忠郎 <tadao@yokoitadaonoiMac.local>
2020-01-04 07:31:33 +0900

    Created Elm Application

---
+++ assets/elm/src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- MAIN
+
+
+main : Html msg
+main =
+  div []
+    [ h1 [][ text "Games" ]
+    , gamesIndex model
+    ]
+
+-- MODEL
+
+
+model : List String
+model =
+  [ "Platform Game"
+  , "Adventure Game"
+  ]
+  
+-- VIEW
+
+
+gamesIndex : List String -> Html msg
+gamesIndex gameTitles =
+  div [ class "games-index" ] [ gamesList gameTitles]
+
+gamesList : List String -> Html msg
+gamesList gameTitles =
+  ul [ class "games-list" ] ( List.map gamesListItem gameTitles )
+
+gamesListItem : String -> Html msg
+gamesListItem gameTitle =
+  li [] [ text gameTitle ]
